7894b818b3a5c12e97547a670f23ffcff64b10fe
Patrick Lin <patrick.cclin@gmail.com>
2024-03-28 22:36:53 +0800

    Update simple_client_03.c

--- project/src/simple_client_03.c
+++ project/src/simple_client_03.c
@@ -3,76 +3,88 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <signal.h>
-#include <time.h>
 #include <modbus/modbus.h>
 
-#define MODBUS_PORT	1503
-#define MODBUS_CONNECTIONS	5
+#include "modbus_ex.h"
 
-#define MODBUS_DATA_POSITION_LED_RED	0
-#define MODBUS_DATA_POSITION_LED_GREEN	1
-#define MODBUS_DATA_POSITION_LED_BLUE	2
-
-int modbusSendData(modbus_t *ctx, int16_t *regs, int nb)
+int writeLed(modbus_t *ctx, POS_ADDR addr, BIT_VALUE val)
 {
-	int rc;
+	int rc = 0;
+	uint8_t read_data;
 	
-	rc = modbus_write_registers(ctx, 1, nb, regs);
-	if (rc != nb) 
-	{
-		printf("ERROR modbus_write_registers (%d)\n", rc);
-		printf("Address = %d, nb = %d\n", 1, nb);
-		return -1;
-	} 
+	rc = modbus_write_bit(ctx, addr, val);
+    if (rc != 1) {
+        printf("FAILED\n");
+        return -1;
+    }
 
-	rc = modbus_read_registers(ctx, 1, nb, regs);
-	if (rc != nb)
-	{
-		printf("ERROR modbus_read_registers (%d)\n", rc);
-		return -1;
-	} 
-
-	return 0;
+    rc = modbus_read_bits(ctx, addr, 1, &read_data);
+    if (rc != 1) {
+        printf("FAILED (nb points %d)\n", rc);
+        return -1;
+    }
+    
+    return 0;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, j;
-	modbus_t *ctx;
-	int16_t regs[10];
-	int nb = sizeof(regs)/sizeof(int16_t);
+    modbus_t *ctx;
+    int rc;
+    int i;
 
 	if( argc != 2 )
 	{
-		printf("USAGE: simple_client_02 SERVERIP\r\n");
+		printf("USAGE: simple_server_03 SERVER_IP\r\n");
 		return -1;
 	}
 	
-	memset(regs, 0, sizeof(regs));
-	
-	// connect to server
-	ctx = modbus_new_tcp(argv[1], MODBUS_PORT); 
-	if (modbus_connect(ctx) == -1) {
-		fprintf(stderr,"Connection failed: %s\n",modbus_strerror(errno));
-		modbus_free(ctx);
-		return -1;
+    ctx = modbus_new_tcp(argv[1], 1502);
+    //modbus_set_debug(ctx, TRUE);
+    modbus_set_error_recovery(ctx,
+                              MODBUS_ERROR_RECOVERY_LINK |
+                              MODBUS_ERROR_RECOVERY_PROTOCOL);
+
+    if (modbus_connect(ctx) == -1) {
+        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+        modbus_free(ctx);
+        return -1;
+    }
+
+    printf("\nTEST WRITE/READ:\n");
+
+	for(i=0; i<10; i++)
+	{
+		printf("*******************************************************************\r\n");
+		printf("Turn on LED_RED\r\n");
+		rc = writeLed(ctx, POS_LED_RED, ON);
+		if( rc < 0 )
+		{
+			printf("Write LED error (%d)\r\n", POS_LED_RED);
+			goto close;
+		}
+		
+		sleep(1);
+
+		printf("*******************************************************************\r\n");
+		printf("Turn off LED_RED\r\n");
+		rc = writeLed(ctx, POS_LED_RED, OFF);
+		if( rc < 0 )
+		{
+			printf("Write LED error (%d)\r\n", POS_LED_RED);
+			goto close;
+		}
+		
+		sleep(1);
 	}
-	modbus_set_debug(ctx, FALSE);
 
-	// write regs
-	regs[MODBUS_DATA_POSITION_LED_RED] = 0;
-	regs[MODBUS_DATA_POSITION_LED_GREEN] = 0;
-	regs[MODBUS_DATA_POSITION_LED_BLUE] = 0;
-	modbusSendData(ctx, regs, nb);
-	
-	regs[MODBUS_DATA_POSITION_LED_RED] = 1;
-	regs[MODBUS_DATA_POSITION_LED_GREEN] = 1;
-	regs[MODBUS_DATA_POSITION_LED_BLUE] = 1;
-	modbusSendData(ctx, regs, nb);
+close:
 
-	return 0;
+    /* Close the connection */
+    modbus_close(ctx);
+    modbus_free(ctx);
+
+    return 0;
 }
 
 // end of file
-
